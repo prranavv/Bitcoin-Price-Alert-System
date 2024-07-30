b34a2ec2ab0cefe6d99bb17c908f175e987e28f4
pranav <109409588+prranavv@users.noreply.github.com>
2024-07-30 20:34:54 +0530

    Changed DB file to ensure the app waits for postgres to run and go online

--- cmd/api/db.go
+++ cmd/api/db.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
-	"os"
+	"log"
 	"time"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
@@ -11,27 +10,37 @@ import (
 
 var DBConn = &DB{}
 
-const maxOpenDbConn = 10
-const maxIdleDbConn = 5
-const maxDblifeTime = 5 * time.Minute
-
-func ConnectDB() (*DB, error) {
-	//Open a connection pool
-	db, err := sql.Open("pgx", "host=localhost port=5432 dbname=postgres user=postgres password=pranav sslmode=disable")
+func openDB(dsn string) (*DB, error) {
+	db, err := sql.Open("pgx", dsn)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		return nil, err
 	}
-	//testing whether the databasea is connected
 	err = db.Ping()
 	if err != nil {
 		return nil, err
 	}
-	//setting constants for the database connections
-	db.SetMaxIdleConns(maxIdleDbConn)
-	db.SetMaxOpenConns(maxOpenDbConn)
-	db.SetConnMaxLifetime(maxDblifeTime)
-	//assigning the DB struct to the database
 	DBConn.SQL = db
 	return DBConn, nil
+}
+
+func ConnectDB() (*DB, error) {
+	dsn := "host=localhost port=5432 user=postgres password=pranav dbname=postgres sslmode=disable timezone=UTC connect_timeout=5"
+	counts := 0
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgres not ready....")
+			counts++
+		} else {
+			log.Println("Connected to Postgres")
+			return connection, nil
+		}
+		if counts > 10 {
+			log.Println(err)
+			return nil, err
+		}
+		log.Println("Backing off for 2 seconds")
+		time.Sleep(2 * time.Second)
+		continue
+	}
 }
